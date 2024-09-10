14b52ee9231625b79d17dccaf29bed2363007dfe
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2024-09-10 11:06:45 -0300

    feat: create a state machine to parse the arguments, it can parse strings, booleans and numbers

--- lib/commands.ex
+++ lib/commands.ex
@@ -48,7 +48,83 @@ defmodule Commands do
         case splited do
           [key, value] -> {:ok, key, value}
           _ -> {:err, "Error parsing SET args"}
+defmodule ParseArgs do
+  @transitions %{
+    0 => %{"\"" => 1, :* => 2, :number => 5},
+    1 => %{:whitespace => 1, "\\" => 3, "\"" => 4, :* => 1},
+    2 => %{:whitespace => 4, :* => 2, :number => 2},
+    3 => %{"\"" => 1, :* => :err},
+    4 => %{},
+    5 => %{:* => 2, :number => 5, " " => 4}
+  }
+
+  @change_type_transitions %{
+    0 => %{1 => :string, 2 => :string, 5 => :number},
+    1 => %{},
+    2 => %{},
+    3 => %{},
+    4 => %{},
+    5 => %{2 => :string}
+  }
+
+  @terminal? %{
+    0 => false,
+    1 => false,
+    2 => false,
+    3 => false,
+    4 => true,
+    5 => false
+  }
+
+  def next_token(str, token \\ "", state \\ 0, type \\ :undefined) do
+    {head, tail} = String.split_at(str, 1)
+    transition_for_state = @transitions[state]
+    type_transitions_for_state = @change_type_transitions[state]
+    # IO.puts("head: #{head}, state: #{state}, token: #{token}, type: #{type}")
+
+    case {@terminal?[state], String.length(head) > 0} do
+      {false, true} ->
+        char_type =
+          cond do
+            head =~ ~r/[\"\\]/ -> head
+            head == " " || head == "\t" -> :whitespace
+            head =~ ~r/[0-9]/ -> :number
+            true -> :*
+          end
+
+        case transition_for_state[char_type] do
+          nil ->
+            {:err, "unable to parse token"}
+
+          result_state ->
+            new_type =
+              case type_transitions_for_state[result_state] do
+                nil -> type
+                result -> result
+              end
+
+            next_token(tail, token <> head, result_state, new_type)
         end
+
+      {true, true} ->
+        {:ok, String.trim(token), String.trim(str),
+         if is_boolean_type(token) do
+           :boolean
+         else
+           type
+         end}
+
+      {_, false} ->
+        {:ok, String.trim(token), "",
+         if is_boolean_type(token) do
+           :boolean
+         else
+           type
+         end}
     end
+  end
+
+  def is_boolean_type(token) do
+    token == "TRUE" || token == "FALSE"
   end
 end
--- test/desafio_cli_test.exs
+++ test/desafio_cli_test.exs
@@ -13,5 +13,28 @@ defmodule DesafioCliTest do
     assert Commands.parse("SET key value") == {:ok, "SET", "key", "value"}
     assert Commands.parse("Set \"key\" value") == {:ok, "SET", "key", "value"}
     assert Commands.parse("SET \"key\" value") == {:ok, "SET", "key", "value"}
+
+  test "Parse string" do
+    assert ParseArgs.next_token("\"teste\" \"algo depois\"") ==
+             {:ok, "\"teste\"", "\"algo depois\"", :string}
+
+    assert ParseArgs.next_token("teste") == {:ok, "teste", "", :string}
+    assert ParseArgs.next_token("teste value") == {:ok, "teste", "value", :string}
+  end
+
+  test "Parse number" do
+    assert ParseArgs.next_token("123") == {:ok, "123", "", :number}
+    assert ParseArgs.next_token("123456") == {:ok, "123456", "", :number}
+    assert ParseArgs.next_token("123abc") == {:ok, "123abc", "", :string}
+    assert ParseArgs.next_token("1a2b3c") == {:ok, "1a2b3c", "", :string}
+  end
+
+  test "Parse boolean" do
+    assert ParseArgs.next_token("TRUE") == {:ok, "TRUE", "", :boolean}
+    assert ParseArgs.next_token("FALSE") == {:ok, "FALSE", "", :boolean}
+    assert ParseArgs.next_token("\"TRUE\"") == {:ok, "\"TRUE\"", "", :string}
+    assert ParseArgs.next_token("\"FALSE\"") == {:ok, "\"FALSE\"", "", :string}
+    assert ParseArgs.next_token("true") == {:ok, "true", "", :string}
+    assert ParseArgs.next_token("false") == {:ok, "false", "", :string}
   end
 end
